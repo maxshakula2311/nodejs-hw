f4f141a28af230c148c1504f330bd399e744ba2b
max-shakula <maxshakulakr@gmail.com>
2026-06-07 01:44:11 +0300

    changed to segments
    forgot abt it again

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,15 +1,16 @@
 import Joi from 'joi';
+import { Segments } from 'celebrate';
 
-export const registerUserSchema = Joi.object({
-  body: Joi.object({
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
   }),
-});
+};
 
-export const loginUserSchema = Joi.object({
-  body: Joi.object({
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
-});
+};
